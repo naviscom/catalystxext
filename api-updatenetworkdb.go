982daa57ceceeb8559aa00000886228cf9caadfc
Ghaz Riz <ghaz.riz@naviscom.com>
2024-02-25 04:48:36 +0500

    new commit

--- api-updatenetworkdb.go
+++ api-updatenetworkdb.go
@@ -13,39 +13,38 @@ import (
 	// "strings"
 	"time"
 
-	"github.com/360EntSecGroup-Skylar/excelize"
+	// "github.com/360EntSecGroup-Skylar/excelize"
 	"github.com/gin-gonic/gin"
 )
 
-type keyResults struct {
-//	rowExcelNew        []string
-	locnRecUpdatedNew  int
-	siteRecUpdatedNew  int
-	cellRecUpdatedNew  int
-	locnRecInsertedNew int
-	siteRecInsertedNew int
-	cellRecInsertedNew int
-}
+// type keyResults struct {
+// //	rowExcelNew        []string
+// 	locnRecUpdatedNew  int
+// 	siteRecUpdatedNew  int
+// 	cellRecUpdatedNew  int
+// 	locnRecInsertedNew int
+// 	siteRecInsertedNew int
+// 	cellRecInsertedNew int
+// }
 
-func excelSheet2(file *excelize.File, sheet string, c chan keyResults) {
-	var rowExcel []string
-	allRowsExcel, err := file.Rows(sheet)
-	if err != nil {
-		fmt.Println(err)
-		// log.Fatal(err)
-		return
-	}
-	rowsProcessed := 0
-	for allRowsExcel.Next() {
-		rowExcel = allRowsExcel.Columns()
-		fmt.Println(rowExcel)
-		rowsProcessed++
-	}
-	close(c)
-}
+// func excelSheet2(file *excelize.File, sheet string, c chan keyResults) {
+// 	var rowExcel []string
+// 	allRowsExcel, err := file.Rows(sheet)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		// log.Fatal(err)
+// 		return
+// 	}
+// 	rowsProcessed := 0
+// 	for allRowsExcel.Next() {
+// 		rowExcel = allRowsExcel.Columns()
+// 		fmt.Println(rowExcel)
+// 		rowsProcessed++
+// 	}
+// 	close(c)
+// }
 
 func  UpdateNetworkDB(ctx *gin.Context) {
-
 	os.Mkdir("/app/downloads", 0700)
 	fileHeader, err := ctx.FormFile("file")
 	if err != nil {
@@ -68,7 +67,7 @@ func  UpdateNetworkDB(ctx *gin.Context) {
 		return
 	}
 	defer networkDB.Close()		
-
+	
 	// //Delete temp file after importing
 	// defer os.Remove("/app/downloads/"+filenamenew)
 
@@ -84,35 +83,36 @@ func  UpdateNetworkDB(ctx *gin.Context) {
 		return
 	}
 	networkDB.Close()
-	///////////////////////////
-	f, err := excelize.OpenFile("/app/downloads/"+filenamenew)
-	if err != nil {
-		println(err.Error())
-		time.Sleep(20 * time.Second)
-	}
-	sheetName2 := f.GetSheetName(2)
+	// ///////////////////////////
+	// f, err := excelize.OpenFile("/app/downloads/"+filenamenew)
+	// if err != nil {
+	// 	println(err.Error())
+	// 	time.Sleep(20 * time.Second)
+	// }
+	// sheetName2 := f.GetSheetName(2)
 
-	ch2 := make(chan keyResults)
-	if sheetName2 == "3G" {
-		fmt.Println("Found 3G as sheet2: ", sheetName2)
-		go excelSheet2(f, sheetName2, ch2)
-	}
-	ch2Count := 0
-	var res2New keyResults
-	for {
-		res2, ok2 := <-ch2
-		if ok2 {
-			ch2Count++
-			res2New = res2
-			//fmt.Println("Ch2-->", ok2, ch2Count, res2)
-		}
-		fmt.Println("ch2Count: ", " locnRecUpdatedNew: ", res2New.locnRecUpdatedNew, " siteRecUpdatedNew: ", res2New.siteRecUpdatedNew, " cellRecUpdatedNew: ", res2New.cellRecUpdatedNew)
-		fmt.Println("ch2Count: ", " locnRecInsertedNew: ", res2New.locnRecInsertedNew, " siteRecInsertedNew: ", res2New.siteRecInsertedNew, " cellRecInsertedNew: ", res2New.cellRecInsertedNew)
-		if !ok2  {
-			break
-		}
-	}
+	// ch2 := make(chan keyResults)
+	// if sheetName2 == "3G" {
+	// 	fmt.Println("Found 3G as sheet2: ", sheetName2)
+	// 	go excelSheet2(f, sheetName2, ch2)
+	// }
+	// ch2Count := 0
+	// var res2New keyResults
+	// for {
+	// 	res2, ok2 := <-ch2
+	// 	if ok2 {
+	// 		ch2Count++
+	// 		res2New = res2
+	// 		//fmt.Println("Ch2-->", ok2, ch2Count, res2)
+	// 	}
+	// 	fmt.Println("ch2Count: ", " locnRecUpdatedNew: ", res2New.locnRecUpdatedNew, " siteRecUpdatedNew: ", res2New.siteRecUpdatedNew, " cellRecUpdatedNew: ", res2New.cellRecUpdatedNew)
+	// 	fmt.Println("ch2Count: ", " locnRecInsertedNew: ", res2New.locnRecInsertedNew, " siteRecInsertedNew: ", res2New.siteRecInsertedNew, " cellRecInsertedNew: ", res2New.cellRecInsertedNew)
+	// 	if !ok2  {
+	// 		break
+	// 	}
+	// }
 	//////////////////////////
 	ctx.JSON(http.StatusOK, "File "+filenamenew+" uploaded successfully")
+	// ctx.JSON(http.StatusOK, "File uploaded successfully")
 	time.Sleep(1 * time.Second)
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/naviscom/catalystxext
 
 go 1.21.5
 
-require (
-	github.com/360EntSecGroup-Skylar/excelize v1.4.1
-	github.com/gin-gonic/gin v1.9.1
-)
+require github.com/gin-gonic/gin v1.9.1
 
 require (
 	github.com/bytedance/sonic v1.9.1 // indirect
@@ -22,7 +19,6 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
